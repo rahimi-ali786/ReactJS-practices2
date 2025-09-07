09b34a62b04eb813dc23538008b86caa3ead715f
rahimi.ali <rahimi.ali@outlook.de>
2025-09-07 11:07:02 +0200

    6.ReactJS - Forms( Uncontrolled Components | Ref )

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 class Login extends Component {
-   handleSubmit = (e) => {
-    e.preventDefault();
-  }
+   email = createRef();
+   password = createRef();
 
+   handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('email:', this.email.current.value);
+    console.log('password:', this.password.current.value);
+    const account = {email: this.email.current.value , password: this.password.current.value};
+    if(account.email && account.password){
+      const response = await axios.post('https://reqres.in/api/login', account, {
+        headers: {
+          'x-api-key': 'reqres-free-v1'
+        }
+      });
+      console.log(response);
+    }   
+  }
+   
  render() { 
   const {timestamp} = this.props;
 
@@ -17,16 +32,16 @@ class Login extends Component {
    <form onSubmit={this.handleSubmit}>
      <div className="mb-3">
        <label htmlFor="email">Email:</label>
-       <input id="email" className="form-control" type="text"/>
+       <input ref={this.email} id="email" className="form-control" type="text"/>
      </div>
       <div className="mb-3">
        <label htmlFor="password">Password:</label>
-       <input id="password" className="form-control" type="text"/>
+       <input ref={this.password} id="password" className="form-control" type="text"/>
      </div>
      <button className="btn btn-primary">Login</button>
    </form>
    </>
-  );
+  ); 
  }
 }
  
